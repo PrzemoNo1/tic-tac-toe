60e9219611e5f6b73342190468e07fe23e909f9a
PrzemoNo1 <37351230+PrzemoNo1@users.noreply.github.com>
2020-02-09 22:10:49 +0100

    v. 1.0 completed

--- src/main.py
+++ src/main.py
@@ -23,9 +23,15 @@ referee = Referee()
 board = Board()
 
 while referee.get_status(board.get_board()) == Status.Ongoing:
+    switch_player()
     display(board.get_board())
+    print("It is " + current_player + '\'s turn.')
     field_number = input("Choose field (1-9): ")
     while not input_check.is_in_range(field_number):
         field_number = input("Wrong value. Choose between from 1 to 9: ")
     board.set_field(field_number, current_player)
-    switch_player()
+
+if referee.get_status(board.get_board()) == Status.Win:
+    print(current_player + ' won!')
+else:
+    print('Game ended with draw.')
